64c11a89ad7e8ce47905ed41f86441e3839081b0
Garrett Girod <girodg@hotmail.com>
2022-02-02 09:31:07 -0500

    Day02

---
+++ Day02/Day02/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Day02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        static void ArrayChallenge()
+        {
+            int[] nums = new int[10];
+            Random random = new Random();
+
+        }
+    }
+}
