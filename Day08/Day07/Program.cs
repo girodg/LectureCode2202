f9ad3fc3359dcd5c10179c640f09ccb602fe6404
Garrett Girod <girodg@hotmail.com>
2022-02-16 11:58:37 -0500

    unit testing

--- Day08/Day07/Program.cs
+++ Day08/Day07/Program.cs
@@ -23,35 +23,63 @@ namespace Day07
             batmobile.CarDetails();//pass in batmobile as "this"
             myRide.CarDetails();
 
-            Inventory backpack = new Inventory(5, new List<string>());
-            backpack.AddItem("sword");
-            backpack.AddItem("staff");
-            backpack.AddItem("bow");
-            backpack.AddItem("potion");
-            backpack.AddItem("arrows");
-            try
-            {
-                backpack.AddItem("cloak");
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine(ex.Message);
-            }
+            Inventory backpack = new Inventory(5, new List<FantasyWeapon>());
+            //backpack.AddItem("sword");
+            //backpack.AddItem("staff");
+            //backpack.AddItem("bow");
+            //backpack.AddItem("potion");
+            //backpack.AddItem("arrows");
+            //try
+            //{
+            //    backpack.AddItem("cloak");
+            //}
+            //catch (Exception ex)
+            //{
+            //    Console.WriteLine(ex.Message);
+            //}
 
             FantasyWeapon sword = new FantasyWeapon(WeaponRarity.Legendary, 100, 5000, 100000);
             int damage = sword.DoDamage();
             Console.WriteLine($"I swing my sword (swish) and do {damage} points of damage to the rat.");
 
             FantasyWeapon fw = WeaponFactory.MakeWeapon(WeaponRarity.Rare, 20, 500, 10000);
+            backpack.AddItem(sword);
+            backpack.AddItem(fw);
+            backpack.AddItem(new BowWeapon(10, 20, WeaponRarity.Common, 2, 20, 5));
+            backpack.PrintInventory();
 
             int number = 5;
             float dNumber = number;//implicit casting
             number = (int)dNumber;//explicit casting
 
             Car tesla = myRide;
+            //UPCASTING
             //always safe
             //car    truck
             myRide = cyber;//upcasting. from Truck to Car
+
+            //DOWNCASTING
+            //NOT SAFE!!
+            //truck  car
+            try
+            {
+                //explicit cast
+                cyber = (Truck)batmobile;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("NOT A TRUCK!");
+            }
+
+            //use the "as" keyword
+            //will set cyber to null if batmobile is not a truck
+            cyber = batmobile as Truck;//will not throw an exception
+            if(cyber != null)
+                Console.WriteLine(cyber.TowingCapacity);
+
+            //use pattern matching with the "is"
+            if (batmobile is Truck trucky)
+                Console.WriteLine(trucky.TowingCapacity);
         }
     }
 }
--- Day08/Day07CL/Inventory.cs
+++ Day08/Day07CL/Inventory.cs
@@ -9,7 +9,7 @@ namespace Day07CL
     public class Inventory
     {
         private int _capacity = 10;
-        private List<string> _items;
+        private List<FantasyWeapon> _items;
 
         public int Capacity
         {
@@ -22,27 +22,37 @@ namespace Day07CL
             get { return _items.Count; }
         }
 
-        public List<string> Items
+        public List<FantasyWeapon> Items
         {
             get { return _items; }
             private set { _items = value; }
         }
 
-        public Inventory(int capacity, List<string> items)
+        public Inventory(int capacity, List<FantasyWeapon> items)
         {
             //capacity = Capacity;//BACKWARDS!
             Capacity = capacity;
             Items = items;//will NOT clone
-            Items = new List<string>(items);//will clone
+            Items = new List<FantasyWeapon>(items);//will clone
             Items = items.ToList();//will clone too!
         }
 
-        public void AddItem(string item)
+        public void AddItem(FantasyWeapon item)
         {
             if (Capacity == Count)
                 throw new Exception("Inventory is full!");
 
             _items.Add(item);
+        }
+
+        public void PrintInventory()
+        {
+            foreach (FantasyWeapon weapon in _items)
+            {
+                Console.WriteLine($"I have a {weapon.Rarity} level {weapon.Level} weapon that can do {weapon.MaxDamage} damage and costs {weapon.Cost}");
+                if (weapon is BowWeapon bow)
+                    Console.WriteLine($"\tIt has {bow.ArrowCount} and can hold {bow.ArrowCapacity} arrows.");
+            }
         }
     }
 }
